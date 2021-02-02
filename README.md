c2c95399713e7469ab994a0dcf81cf8d107c5baf
kleyla <kleyla01@live.com>
2021-02-01 22:31:38 -0400

    useLayoutEffect, useMemo, useCallback

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ NO se pueden usar hook de manera condicional
 ## useRef
 
 Para mantener una referencia mutable
+
+## memo
+
+Para memorizar algo y que solo sus properties cambian, va a volverse a memorizar, caso contrario usara la version memorizada cuando tenga que redibujar.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CallbackHook } from "./components/05-memos/CallbackHook";
+// import { MemoHook } from "./components/05-memos/MemoHook";
 // import { FocusScreen } from "./components/03-useRef/FocusScreen";
-import { RealExampleUseRef } from "./components/03-useRef/RealExampleUseRef";
+// import { RealExampleUseRef } from "./components/03-useRef/RealExampleUseRef";
+// import { Layout } from "./components/04-useLayoutEffect/Layout";
+// import { Memorize } from "./components/05-memos/Memorize";
 // import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./components/examples/MultipleCustomHooks";
 // import { CounterApp } from "./components/01-useState/CounterApp";
@@ -9,4 +13,4 @@ import { RealExampleUseRef } from "./components/03-useRef/RealExampleUseRef";
 // import { HookApp } from "./HookApp";
 // import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 
-ReactDOM.render(<RealExampleUseRef />, document.getElementById("root"));
+ReactDOM.render(<CallbackHook />, document.getElementById("root"));
